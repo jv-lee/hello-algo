35772b88b7da76d68f6f187e0f18cc311ed7050a
lijiawei11 <lijiawei11@baidu.com>
2023-06-27 10:48:37 +0800

    update code

--- src/test/Test.java
+++ src/test/Test.java
@@ -14,7 +14,6 @@ public class Test {
 
     public static int[] towSum1(int[] nums, int target) {
         int size = nums.length;
-
         for (int i = 0; i < size - 1; i++) {
             for (int j = i + 1; j < size; j++) {
                 if (nums[i] + nums[j] == target) {
@@ -22,14 +21,12 @@ public class Test {
                 }
             }
         }
-
         return new int[0];
     }
 
     public static int[] towSum2(int[] nums, int target) {
         int size = nums.length;
         Map<Integer, Integer> map = new HashMap<>();
-
         for (int i = 0; i < size; i++) {
             if (map.containsKey(target - nums[i])) {
                 return new int[] { map.get(target - nums[i]), i };
