0fe7accf15ea789c461422925b528a7424dc5737
lijiawei11 <lijiawei11@baidu.com>
2023-12-25 18:07:51 +0800

    update sort test code

--- src/test/SortTest.java
+++ src/test/SortTest.java
@@ -5,23 +5,45 @@ import java.util.Arrays;
 public class SortTest {
     public static void test() {
         bubbleSort();
+        insetionSort();
     }
 
     public static void bubbleSort() {
         int[] array = { 6, 4, 3, 5, 6, 7, 2, 1 };
 
-        for (int i =0;i<array.length;i++) {
+        for (int i = 0; i < array.length; i++) {
             boolean flag = false;
-            for (int j = 0;j<array.length - i - 1;j ++) {
-                if (array[j] > array[j+1]) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
                     int temp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = temp;
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
                     flag = true;
                 }
             }
-            if (flag == false) break;
+            if (flag == false)
+                break;
         }
+
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void insetionSort() {
+        int[] array = { 6, 4, 3, 5, 6, 7, 2, 1 };
+
+        for (int i = 1; i < array.length; i++) {
+            int value = array[i];
+            int j = i - 1;
+            for (; j >= 0; j--) {
+                if (array[j] > value) {
+                    array[j + 1] = array[j];
+                } else {
+                    break;
+                }
+            }
+            array[j + 1] = value;
+        }
+
         System.out.println(Arrays.toString(array));
     }
 }
