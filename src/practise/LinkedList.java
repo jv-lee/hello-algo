09cfd884a4ea05595146ecd6586ea262496adda3
jv.lee <jv.lee@foxmail.com>
2024-04-17 10:21:22 +0800

    add leetcode 21.

--- src/practise/LinkedList.java
+++ src/practise/LinkedList.java
@@ -46,6 +46,29 @@ public class LinkedList {
 
         return true;
     }
+
+    /**
+     * 21. 合并两个有序链表
+     * 使用链表大小比较法
+     * 时间复杂度O(n + m) 两个链表的长度
+     * 空间复杂度O(1)
+     */
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode temphead = new ListNode(-1);
+        ListNode cur = temphead;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                cur.next = list1;
+                list1 = list1.next;
+            } else {
+                cur.next = list2;
+                list2 = list2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = list1 == null ? list2 : list1;
+        return temphead.next;
+    }
     
 
 }
