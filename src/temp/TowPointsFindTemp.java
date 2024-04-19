a2639cce81db387f0a2c5cece3faa2101ef00245
jv.lee <jv.lee@foxmail.com>
2024-04-19 09:52:39 +0800

    add TowPointsFindTemp code.

---
+++ src/temp/TowPointsFindTemp.java
@@ -0,0 +1,65 @@
+package temp;
+
+public class TowPointsFindTemp {
+        /// 704. 二分查找
+        public int search(int[] nums, int target) {
+            return -1;
+        }
+    
+        /// 374. 猜数字大小
+        public int guessNumber(int n) {
+            return -1;
+        }
+    
+        /// 35. 搜索插入位置
+        public int searchInsert(int[] nums, int target) {
+            return -1;
+        }
+    
+        /// 34. 在排序数组中查找元素的第一个和最后一个位置
+        public int[] searchRange(int[] nums, int target) {
+            return new int[]{};
+        }
+    
+        /// 153. 寻找旋转排序数组中的最小值
+        public int findMin(int[] nums) {
+            return -1;
+        }
+    
+        /// 154. 寻找旋转排序数组中的最小值 II
+        public int findMin(int[] nums) {
+            return -1;
+        }
+
+        /// 33. 搜索旋转排序数组
+        public int search(int[] nums, int target) {
+            return -1;
+        }
+
+        /// 81. 搜索旋转排序数组 II
+        public int search(int[] nums, int target) {
+            return -1;
+        }
+
+        /// 278. 第一个错误的版本
+        /// The isBadVersion API is defined in the parent class VersionControl. boolean isBadVersion(int version);
+        public int firstBadVersion(int n) {
+            return - 1;
+        }
+
+        /// LCR 069. 山脉数组的峰顶索引
+        public int peakIndexInMountainArray(int[] arr) {
+            return -1;
+        }
+
+        /// 1095. 山脉数组中查找目标值
+        public int findInMountainArray(int target, MountainArray mountainArr) {
+            return -1;
+        }
+
+        /// 69. x 的平方根 
+        public int mySqrt(int x) {
+            return -1;
+        }
+
+}
