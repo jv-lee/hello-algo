cf5b8acaf2170a16e49bb3b6b502d8b5cdd2e45b
lijw17 <lijw17@xiaopeng.com>
2024-04-22 10:16:43 +0800

    add LinkedListTemp code.

---
+++ src/temp/LinkedListTemp.java
@@ -0,0 +1,35 @@
+package temp;
+
+import data.ListNode;
+
+public class LinkedListTemp {
+    /// 206. 反转链表
+    public ListNode reverseList(ListNode head) {
+
+    }
+
+    /// 141. 环形链表
+    public boolean hasCycle(ListNode head) {
+
+    }
+
+    /// 21. 合并两个有序链表
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+    }
+
+    /// 876. 链表的中间结点
+    public ListNode middleNode(ListNode head) {
+
+    }
+
+    /// 19. 删除链表的倒数第 N 个结点
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+    }
+
+    /// 61. 旋转链表
+    public ListNode rotateRight(ListNode head, int k) {
+
+    }
+}
