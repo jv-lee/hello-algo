ce39c530aad322dcf2e3b2f953ddc93ff8fb86c9
jv-lee <jv.lee@foxmail.com>
2024-04-29 14:42:16 +0800

    add leetcode 34.

--- src/practise/TowPointsFind.java
+++ src/practise/TowPointsFind.java
@@ -60,4 +60,49 @@ public class TowPointsFind {
         return left;
     }
 
+    /// 34. 在排序数组中查找元素的第一个和最后一个位置
+    /// 时间复杂度O(logN)
+    /// 空间复杂度O(1)
+    public int[] searchRange(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 0) {
+            return new int[] { -1, -1 };
+        }
+
+        int firstIndex = findFirstIndex(nums, target);
+        if (firstIndex == -1) {
+            return new int[] { -1, -1 };
+        }
+        int lastIndex = findLastIndex(nums, target);
+        return new int[] { firstIndex, lastIndex };
+    }
+
+    public int findFirstIndex(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left] == target ? left : -1;
+    }
+
+    public int findLastIndex(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left + 1) / 2;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid;
+            }
+        }
+        return nums[left] == target ? left : -1;
+    }
+
 }
