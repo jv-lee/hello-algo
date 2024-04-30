a096d15ceff33ed49158a7d9abad7ab248f104af
jv-lee <jv.lee@foxmail.com>
2024-04-30 10:32:36 +0800

    add leetcode 153.

--- src/practise/TowPointsFind.java
+++ src/practise/TowPointsFind.java
@@ -105,4 +105,21 @@ public class TowPointsFind {
         return nums[left] == target ? left : -1;
     }
 
+    /// 153. 寻找旋转排序数组中的最小值
+    /// 时间复杂度O(logN)
+    /// 空间复杂度O(1)
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+
 }
