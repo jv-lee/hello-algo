8882eb50b831c4b0b976436eab2ec0e686eac1d7
jv-lee <jv.lee@foxmail.com>
2024-05-13 18:29:19 +0800

    add leetcode 81.

--- src/practise/TowPointsFind.java
+++ src/practise/TowPointsFind.java
@@ -144,7 +144,7 @@ public class TowPointsFind {
     /// 33. 搜索旋转排序数组
     /// 时间复杂度O(logN)
     /// 空间复杂度O(1)
-    public int search2(int[] nums, int target) {
+    public int searchRotate(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
         while (left <= right) {
@@ -166,6 +166,37 @@ public class TowPointsFind {
             }
         }
         return -1;
+    }
+
+    /// 81. 搜索旋转排序数组 II
+    /// 时间复杂度O(logN)
+    /// 空间复杂度O(1)
+    public boolean searchRotate2(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return true;
+            } else if (nums[left] == nums[mid]) {
+                left++;
+            } else if (nums[right] == nums[mid]) {
+                right--;
+            } else if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[right] >= target && target > nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return false;
     }
 
 }
