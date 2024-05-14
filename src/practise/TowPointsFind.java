8c8260a73e798ca93f33b561bde5b1affeeae7ab
jv-lee <jv.lee@foxmail.com>
2024-05-14 18:35:02 +0800

    add leetcode 278.

--- src/practise/TowPointsFind.java
+++ src/practise/TowPointsFind.java
@@ -199,4 +199,23 @@ public class TowPointsFind {
         return false;
     }
 
+    /// 278. 第一个错误的版本
+    /// 时间复杂度 O(logN)
+    /// 空间复杂度 O(1)
+    /// The isBadVersion API is defined in the parent class VersionControl. boolean
+    /// isBadVersion(int version);
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
 }
