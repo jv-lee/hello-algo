52b2c4b8dc3b615f718d085565d251212d5d73c2
jv-lee <jv.lee@foxmail.com>
2024-05-22 17:33:50 +0800

    add leetcode 1.

--- src/practise/Sort.java
+++ src/practise/Sort.java
@@ -1,5 +1,7 @@
 package practise;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class Sort {
@@ -54,6 +56,20 @@ public class Sort {
         int temp = nums[left];
         nums[left] = nums[right];
         nums[right] = temp;
+    }
+
+    /// 1. 两数之和
+    /// 时间复杂度 O(n)
+    /// 空间复杂度 O(n)
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[] { map.get(target - nums[i]), i };
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] { 1 };
     }
 
 }
