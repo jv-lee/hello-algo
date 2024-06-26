8b8b78d878a198fa103da8d7fd493a3070cf3030
jv-lee <jv.lee@foxmail.com>
2024-06-26 11:29:26 +0800

    add leetcode 88.

--- src/practise/Sort.java
+++ src/practise/Sort.java
@@ -88,4 +88,22 @@ public class Sort {
         }
     }
 
+    /// 88. 合并两个有序数组
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int total = m + n - 1;
+        int p1 = m - 1;
+        int p2 = n - 1;
+        while (total >= 0) {
+            if (p1 == -1) {
+                nums1[total--] = nums2[p2--];
+            } else if (p2 == -1) {
+                nums1[total--] = nums1[p1--];
+            } else if (nums1[p1] > nums2[p2]) {
+                nums1[total--] = nums1[p1--];
+            } else {
+                nums1[total--] = nums2[p2--];
+            }
+        }
+    }
+
 }
