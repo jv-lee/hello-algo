b6ef1706629babb46a31df9ea2da44a748af1f94
jv-lee <jv.lee@foxmail.com>
2024-06-27 16:24:39 +0800

    add leetcode 153.

--- src/practise/Sort.java
+++ src/practise/Sort.java
@@ -106,4 +106,22 @@ public class Sort {
         }
     }
 
+    /// 153. 寻找旋转排序数组中的最小值
+    public int findMin(int[] nums) {
+        return findMin(nums, 0, nums.length - 1);
+    }
+
+    public int findMin(int[] nums, int left, int right) {
+        if (left == right) {
+            return nums[left];
+        }
+
+        int mid = left + (right - left) / 2;
+        if (nums[mid] > nums[right]) {
+            return findMin(nums, mid + 1, right);
+        } else {
+            return findMin(nums, left, mid);
+        }
+    }
+
 }
