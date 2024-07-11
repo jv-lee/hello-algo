c13bea0e5b29c2f66c8ab05874e3ae981233dd17
jv-lee <jv.lee@foxmail.com>
2024-07-11 11:14:39 +0800

    add test code.

--- src/temp/TowPointsFindTemp.java
+++ src/temp/TowPointsFindTemp.java
@@ -27,44 +27,36 @@ public class TowPointsFindTemp {
 
     /// 153. 寻找旋转排序数组中的最小值
     public int findMin(int[] nums) {
-        return -1;
     }
 
     /// 154. 寻找旋转排序数组中的最小值 II
     public int findMin(int[] nums) {
-        return -1;
     }
 
     /// 33. 搜索旋转排序数组
     public int search(int[] nums, int target) {
-        return -1;
     }
 
     /// 81. 搜索旋转排序数组 II
-    public int search(int[] nums, int target) {
-        return -1;
+    public boolean search(int[] nums, int target) {
     }
 
     /// 278. 第一个错误的版本
     /// The isBadVersion API is defined in the parent class VersionControl. boolean
     /// isBadVersion(int version);
     public int firstBadVersion(int n) {
-        return -1;
     }
 
     /// LCR 069. 山脉数组的峰顶索引
     public int peakIndexInMountainArray(int[] arr) {
-        return -1;
     }
 
     /// 1095. 山脉数组中查找目标值
     public int findInMountainArray(int target, MountainArray mountainArr) {
-        return -1;
     }
 
     /// 69. x 的平方根
     public int mySqrt(int x) {
-        return -1;
     }
 
 }
