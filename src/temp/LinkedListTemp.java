0ba730f72c10cadd8e9b0cee9578841e3a97bcb8
jv-lee <jv.lee@foxmail.com>
2024-07-29 12:53:46 +0800

    add linkedlist test code.

--- src/temp/LinkedListTemp.java
+++ src/temp/LinkedListTemp.java
@@ -5,25 +5,106 @@ import data.ListNode;
 public class LinkedListTemp {
     /// 206. 反转链表
     public ListNode reverseList(ListNode head) {
+        ListNode cur = head;
+        ListNode prev = null;
+        ListNode temp = null;
+        while (cur != null) {
+            temp = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = temp;
+        }
+        return prev;
     }
 
     /// 141. 环形链表
     public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode s = head;
+        ListNode f = head.next;
+        while (s != f) {
+            if (f == null || f.next == null) {
+                return false;
+            }
+            s = s.next;
+            f = f.next.next;
+        }
+        return true;
     }
 
     /// 21. 合并两个有序链表
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode temphead = new ListNode(-1);
+        ListNode cur = temphead;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                cur.next = list1;
+                list1 = list1.next;
+            } else {
+                cur.next = list2;
+                list2 = list2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = list1 == null ? list2 : list1;
+        return temphead.next;
     }
 
     /// 876. 链表的中间结点
     public ListNode middleNode(ListNode head) {
+        ListNode s = head;
+        ListNode f = head;
+        while (f != null && f.next != null) {
+            s = s.next;
+            f = f.next.next;
+        }
+        return s;
     }
 
     /// 19. 删除链表的倒数第 N 个结点
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        int length = getLength(head);
+        ListNode temphead = new ListNode(-1, head);
+        ListNode cur = temphead;
+        for (int i = 0; i < length - n; i++) {
+            cur = cur.next;
+        }
+        cur.next = cur.next.next;
+        return temphead.next;
+    }
+
+    public int getLength(ListNode head) {
+        int n = 0;
+        while (head != null) {
+            n++;
+            head = head.next;
+        }
+        return n;
     }
 
     /// 61. 旋转链表
     public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        int n = 1;
+        ListNode cur = head;
+        while (cur.next != null) {
+            n++;
+            cur = cur.next;
+        }
+        cur.next = head;
+
+        int move = n - k % n;
+        while (move -- > 0) {
+            cur = cur.next;
+        }
+        ListNode result = cur.next;
+        result.next = null;
+        return result;
     }
 }
