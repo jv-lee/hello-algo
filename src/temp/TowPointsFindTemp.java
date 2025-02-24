a41bb953ec6ce03c5cf74a10a7d066cae15ed298
jv-lee <jv.lee@foxmail.com>
2025-02-24 17:42:18 +0800

    add towPointsFind test code.

--- src/temp/TowPointsFindTemp.java
+++ src/temp/TowPointsFindTemp.java
@@ -13,52 +13,269 @@ public class TowPointsFindTemp {
 
     /// 704. 二分查找
     public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left] == target ? left : -1;
     }
 
     /// 374. 猜数字大小
     public int guessNumber(int n) {
+        int left = 0;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (guess(mid) > 0) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return guess(left) == 0 ? left : -1;
     }
 
     /// 35. 搜索插入位置
     public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
     }
 
     /// 34. 在排序数组中查找元素的第一个和最后一个位置
     public int[] searchRange(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 0) {
+            return new int[]{-1, -1};
+        }
+        int firstIndex = findFirstIndex(nums, target);
+        if (firstIndex == -1) {
+            return new int[]{-1, -1};
+        }
+        int lastIndex = findLastIndex(nums, target);
+        return new int[]{firstIndex, lastIndex};
+    }
+
+    public int findFirstIndex(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left] == target ? left : -1;
+    }
+
+    public int findLastIndex(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left + 1) / 2;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid;
+            }
+        }
+        return nums[left] == target ? left : -1;
     }
 
     /// 153. 寻找旋转排序数组中的最小值
     public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == nums[right]) {
+                right--;
+            } else if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left];
     }
 
     /// 154. 寻找旋转排序数组中的最小值 II
     public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == nums[right]) {
+                right--;
+            } else if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left];
     }
 
     /// 33. 搜索旋转排序数组
     public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[right] >= target && target > nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
     }
 
     /// 81. 搜索旋转排序数组 II
     public boolean search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return true;
+            } else if (nums[mid] == nums[left]) {
+                left++;
+            } else if (nums[mid] == nums[right]) {
+                right--;
+            } else if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[right] >= target && target > nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return false;
     }
 
     /// 278. 第一个错误的版本
     /// The isBadVersion API is defined in the parent class VersionControl. boolean
     /// isBadVersion(int version);
     public int firstBadVersion(int n) {
+        int left = 0;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
     }
 
     /// LCR 069. 山脉数组的峰顶索引
     public int peakIndexInMountainArray(int[] arr) {
+        int left = 0;
+        int right = arr.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] > arr[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
     }
 
     /// 1095. 山脉数组中查找目标值
     public int findInMountainArray(int target, MountainArray mountainArr) {
+        int maxIndex = findMaxIndex(mountainArr);
+        int left = 0;
+        int right = maxIndex;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (mountainArr.get(mid) < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        if (mountainArr.get(left) == target) {
+            return left;
+        }
+        left = maxIndex + 1;
+        right = mountainArr.length() - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (mountainArr.get(mid) > target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return mountainArr.get(left) == target ? left : -1;
+    }
+
+    public int findMaxIndex(MountainArray mountainArr) {
+        int left = 0;
+        int right = mountainArr.length() - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (mountainArr.get(mid) > mountainArr.get(mid + 1)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
     }
 
     /// 69. x 的平方根
     public int mySqrt(int x) {
+        int left = 0;
+        int right = x;
+        int pos = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if ((long) mid * mid <= x) {
+                left = mid + 1;
+                pos = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return pos;
     }
 
 }
